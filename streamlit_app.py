7203772e582e44cb5b86a2de9d75f1b99d2b3615
Akash Das <akash.das@springml.com>
2022-06-24 15:19:20 -0700

    Update streamlit_app.py
    fixes typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # show json as dataframe
 streamlit.dataframe(fruityvice_normalized)
