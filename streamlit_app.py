bf32637b04a857d40f6740338997e42e92172d29
Akash Das <akash.das@springml.com>
2022-06-24 16:34:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("My fruit load list contains: ")
 streamlit.dataframe(my_data_row)
-st.text(pd.DataFrame(my_data_row))
+st.text(my_data_row)
 
 select_fruit = streamlit.multiselect("What fruit would you like to add ?", list(my_data_row), ["Banana"])
