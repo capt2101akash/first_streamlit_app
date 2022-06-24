0fee4514df2f464712d1087e8cc606e225f8616a
Akash Das <akash.das@springml.com>
2022-06-24 15:15:47 -0700

    Update streamlit_app.py
    adds api call to fda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd 
+import requests
 
 streamlit.header('Breakfast Menu')
 streamlit.text(':chicken: Omega 3 & Blueberry Oatmeal')
@@ -12,3 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_show = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_show]
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
