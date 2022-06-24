b22d26728f69c0a388b2a960ca57cbdf087be616
Akash Das <akash.das@springml.com>
2022-06-24 11:02:37 -0700

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.datafrae(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
