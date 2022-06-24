860b9dc77e58767351257738133850254a3d1c9b
Akash Das <akash.das@springml.com>
2022-06-24 11:02:14 -0700

    Update streamlit_app.py
    adds dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+import pandas as pd 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text(':chicken: Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.datafrae(my_fruit_list)
